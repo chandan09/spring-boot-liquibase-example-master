32cb6930cae98a70dd10dbac203d3237a34160c7
chandan09 <31616994+chandan09@users.noreply.github.com>
2021-12-09 13:17:37 +0530

    modifying sql

--- src/main/resources/db/changelog/create-test1-schema.sql
+++ src/main/resources/db/changelog/create-test1-schema.sql
@@ -8,8 +8,8 @@ create table test1 (
 --rollback drop table test1; 
 
 --changeset nvoxland:2 
-insert into test1 (id, name) values (1, ‘name 1′);
-insert into test1 (id,  name) values (2, ‘name 2′);  
+insert into test1 (id, name) values (1, ‘name1′);
+insert into test1 (id,  name) values (2, ‘name2′);  
 
 --changeset nvoxland:3 dbms:mysql
 create sequence seq_test;
